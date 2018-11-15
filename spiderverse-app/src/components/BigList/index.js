781e05da4fb0b61990ed549f8560791d7cf33fb4
CarlosAznable <carabdu@gmail.com>
2018-11-15 18:09:03 -0500

    things are rendering

--- spiderverse-app/src/components/BigList/index.js
+++ spiderverse-app/src/components/BigList/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function BigList(props){
-  debugger;
   // const list = props.listData;
   // const path = list[0].thumbnail.path;
   // const ext = list[0].thumbnail.extension;
@@ -17,14 +16,21 @@ export default function BigList(props){
   // )
 
 const list = props.listData;
-list.map(image => {
+const images = list.map(image => {
   const path = image.thumbnail.path;
   const ext = image.thumbnail.extension;
   return (
     <div key= {image.id}>
       <h3>{image.name}</h3>
-      <img src={path.ext} />
+      <img src={`${path}.${ext}`} />
         </div>
       )
 })
+return (
+  <div>
+  {images}
+  </div>
+)
+
+
 }
